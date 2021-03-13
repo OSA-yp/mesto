4f922dabb16e2c52c18b43ac2c1b3e371c7de3d4
Sergey Okun <okunSA@yandex.ru>
2021-03-13 12:59:01 +0300

    feat: добавить интерактив попапа

---
+++ scripts/index.js
@@ -0,0 +1,72 @@
+
+// событие по клик на кнопке редактирования профиля
+let editButton = document.querySelector('.profile__edit-button');
+editButton.addEventListener('click', openPopup);
+
+// событие по клику на крестик попапа
+let closeButton = document.querySelector('.popup__close');
+closeButton.addEventListener('click', closePopup);
+
+// событие по кнопке сохранить
+let popupForm = document.querySelector('.popup__form');
+popupForm.addEventListener('submit', formSubmitHandler);
+
+// обработчик сабмита
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  savePopup();  // закрываем попап с сохранением
+}
+
+
+
+
+// открыть попап
+function openPopup(){
+  let popup = document.querySelector('.popup');
+
+  let nameForEdit = document.querySelector('.profile__name');
+  let nameInPopup = document.querySelector('.popup__name');
+
+  nameInPopup.value = nameForEdit.textContent;
+
+
+  let jobForEdit = document.querySelector('.profile__job');
+  let jobInPopup = document.querySelector('.popup__job');
+
+  jobInPopup.value = jobForEdit.textContent;
+
+  popup.classList.remove('popup_hiden');
+
+}
+
+// закрыть попап с сохранением
+function savePopup(){
+
+  let popup = document.querySelector('.popup');
+
+  let nameForEdit = document.querySelector('.profile__name');
+  let nameInPopup = document.querySelector('.popup__name');
+
+  nameForEdit.textContent = nameInPopup.value;
+
+
+  let jobForEdit = document.querySelector('.profile__job');
+  let jobInPopup = document.querySelector('.popup__job');
+
+  jobForEdit.textContent = jobInPopup.value;
+
+
+  popup.classList.add('popup_hiden');
+}
+
+// закрыть попап без сохранения
+function closePopup(){
+  let popup = document.querySelector('.popup');
+  popup.classList.add('popup_hiden');
+}
+
+
+
+
+
+
